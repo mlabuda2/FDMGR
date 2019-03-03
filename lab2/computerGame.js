e5427beaa1f7e8f374dd0ba35f69f017149560cf
mlabuda2 <labuda1996@wp.pl>
2019-03-03 11:08:28 +0100

    delete done

--- lab2/computerGame.js
+++ lab2/computerGame.js
@@ -4,11 +4,10 @@ createGame("FIFA 19", "Sport", 2018, true, true, ['multiplayer', 'esport', 'ics'
 createGame("League of Legends", "MMO/RPG", 2009, false, false, ['strategy', 'esport', 'pc'])
 createGame("Wiedźmin 3: Dziki Gon", "RPG", 2015, false, false, ['pc', 'gameoftheyear', ''])
 
-
 console.log(readGame(0))
 
 function createGame(name, category, relase, mobileVersion, payToWin, tags){
-    var computer_games = {
+    var computer_game = {
         name: name,
         category: category,
         relase: relase,
@@ -16,13 +15,17 @@ function createGame(name, category, relase, mobileVersion, payToWin, tags){
         payToWin: payToWin,
         tags: tags
     };
-    db.push(computer_games)
+    db.push(computer_game)
 };
 function readGame(index){
     return db[index]
 }
-function updateGame(){}
-function deleteGame(){}
+function updateGame(index){
+
+}
+function deleteGame(index){
+    delete db[index]
+}
 function findBy(){}
 
 console.log(db)
