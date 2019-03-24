4bb5cac4eb089c92d2abdfcf1955d8ebd45bc977
mlabuda2 <labuda1996@wp.pl>
2019-03-24 09:40:25 +0100

    update lib module

--- lab4/module_inheritance.js
+++ lab4/module_inheritance.js
@@ -1,14 +1,17 @@
+// ========== Zadanie 1 ===========  //
 var lib = {};
 lib.module = (function () {
 
   var private_db = [];
-
-  var privateFunction = function () {};
+  // var isInDatabase = function () {
+  //   sprawdz czy jest w bazie
+  // };
 
   return {
     publicProperty: 1,
-    createGame(id, name, category = null, relase = null, mobileVersion = null, payToWin = null, tags = []) {
+    createGame: function (id, name, category = null, relase = null, mobileVersion = null, payToWin = null, tags = []) {
       if ((!name) || (!id && id != 0) || (!Number.isInteger(id))) { console.log("Bad data."); return false }
+      // isInDatabase()
       var computer_game = {
           id: id,
           name: name,
@@ -20,8 +23,9 @@ lib.module = (function () {
       };
       console.log("Item created.")
       private_db.push(computer_game)
+      return true
     },
-    readGameBy(idx, searchBy) {
+    readGameBy: function (idx, searchBy) {
       var searchedItem = private_db.map(function (item) {
           dispatcher = {
               "id": item.id,
@@ -41,7 +45,7 @@ lib.module = (function () {
       console.log("Item doesn't exist.")
       return null
     },
-    updateGame(updatedGame, newObject) {
+    updateGame: function (updatedGame, newObject) {
       if (updatedGame) {
           var updatedItem = private_db.map(function (item) { return item.id; }).indexOf(updatedGame.id);
       }
@@ -57,7 +61,7 @@ lib.module = (function () {
       console.log("Updated item doesn't exist.")
       return false
     },
-    deleteGameBy(idx, searchBy) {
+    deleteGameBy: function (idx, searchBy) {
       var removeIndex = private_db.map(function (item) {
           dispatcher = {
               "id": item.id,
@@ -78,7 +82,7 @@ lib.module = (function () {
       console.log("Item doesn't exist.")
       return false
     },
-    filterBy(searchBy, value) {
+    filterBy: function (searchBy, value) {
         var filtered = private_db.filter(function (element, index, array) {
           dispatcher = {
               "id": element.id,
@@ -105,15 +109,25 @@ lib.module = (function () {
         });
         return filtered
     },
-    privilegedMethod: function (arg) {
-      console.log(privateDb)
+    readAllDb: function () {
+      return private_db
     }
   };
 })();
 
 lib.module.createGame(0, "FIFA 19", "Sport", 2018, true, true, ['multiplayer', 'esport', 'ics', 'ps4', 'xbox', 'pc'])
 lib.module.createGame(1, "League of Legends", "RPG", 2009, false, false, ['strategy', 'esport', 'pc'])
-lib.module.filterBy("category", "RPG")
-lib.module.filterBy("tags", "ps4")
+lib.module.createGame(2, "Wiedźmin 3: Dziki Gon", "RPG", 2015, false, false, ['pc', 'gameoftheyear', 'ps4'])
+
+game = lib.module.readGameBy("Wiedźmin 3: Dziki Gon", "name")
+game.category = "MMO/RPG"
+console.log(lib.module.updateGame(lib.module.readGameBy("Wiedźmin 3: Dziki Gon", "name"), game))
+
+console.log(lib.module.filterBy("category", "RPG"))
+console.log(lib.module.filterBy("tags", "ps4"))
+
 lib.module.deleteGameBy("League of Legends", "name")
-lib.module.readGameBy("Wiedźmin 3: Dziki Gon", "name")
+
+console.log(lib.module.readAllDb())
+
+// ========== Zadanie 2 ===========  //
