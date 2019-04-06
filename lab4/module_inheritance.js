ab2909ac84d2b83e24f356e8fa7653f3d62e0a7a
mlabuda2 <labuda1996@wp.pl>
2019-04-06 10:52:09 +0200

    up added public function to module

--- lab4/module_inheritance.js
+++ lab4/module_inheritance.js
@@ -3,15 +3,23 @@ var lib = {};
 lib.module = (function () {
 
   var private_db = [];
-  // var isInDatabase = function () {
-  //   sprawdz czy jest w bazie
-  // };
+  var isInDatabase = function (name) {
+    var exists = false;
+    private_db.forEach(element => {
+        if (element.name == name){
+            exists = true;
+        }
+    })
+    return exists;
+  };
 
   return {
     publicProperty: 1,
     createGame: function (id, name, category = null, relase = null, mobileVersion = null, payToWin = null, tags = []) {
-      if ((!name) || (!id && id != 0) || (!Number.isInteger(id))) { console.log("Bad data."); return false }
-      // isInDatabase()
+      if ((!name) || (!id && id != 0) || (!Number.isInteger(id))) { console.log("Bad data."); return false; }
+      if (isInDatabase(name)){
+        return false;
+      }
       var computer_game = {
           id: id,
           name: name,
@@ -23,7 +31,7 @@ lib.module = (function () {
       };
       console.log("Item created.")
       private_db.push(computer_game)
-      return true
+      return true;
     },
     readGameBy: function (idx, searchBy) {
       var searchedItem = private_db.map(function (item) {
@@ -110,12 +118,13 @@ lib.module = (function () {
         return filtered
     },
     readAllDb: function () {
-      return private_db
+      return private_db;
     }
   };
 })();
 
 lib.module.createGame(0, "FIFA 19", "Sport", 2018, true, true, ['multiplayer', 'esport', 'ics', 'ps4', 'xbox', 'pc'])
+lib.module.createGame(3, "FIFA 19", "Sport", 2018, true, true, ['multiplayer', 'esport', 'ics', 'ps4', 'xbox', 'pc'])
 lib.module.createGame(1, "League of Legends", "RPG", 2009, false, false, ['strategy', 'esport', 'pc'])
 lib.module.createGame(2, "Wiedźmin 3: Dziki Gon", "RPG", 2015, false, false, ['pc', 'gameoftheyear', 'ps4'])
 
@@ -162,26 +171,6 @@ ComputerGame.prototype = {
 
 }
 
-// ComputerGame.prototype.getName = function(){
-//     return this.name;
-// }
-
-// ComputerGame.prototype.getWeight = function(){
-//     return this.weight;
-// }
-
-// ComputerGame.prototype.getType = function(){
-//     return this.type;
-// }
-
-// ComputerGame.prototype.getIsExo = function(){
-//     return this.isExo;
-// }
-
-// ComputerGame.prototype.getNamesOfMoons = function(){
-//     return this.namesOfMoons;
-// }
-
 ComputerGame.prototype.getDescription = function(){
     console.log("Planet");
 }
@@ -196,6 +185,19 @@ function MobileComputerGame(name, category, relase, payToWin, tags, is_android, 
 MobileComputerGame.prototype = Object.create(ComputerGame.prototype);
 MobileComputerGame.prototype.constructor = MobileComputerGame;
 
-MobileComputerGame.prototype.getDescription = function(){
-    console.log("Mobile computer game");
+MobileComputerGame.prototype = {
+    is_android: function(){
+        return this.is_android;
+    },
+    is_ios: function (){
+        return this.is_ios;
+    },
+    is_multiplatform: function(){
+        if (this.is_android && this.is_ios) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
