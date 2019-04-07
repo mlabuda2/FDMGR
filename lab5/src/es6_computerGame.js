cdb5e98ff9330d28b2cf20b7fa442a9cb8351885
mlabuda2 <labuda1996@wp.pl>
2019-04-07 09:41:22 +0200

    added geters

---
+++ lab5/src/es6_computerGame.js
@@ -0,0 +1,53 @@
+class ComputerGame {
+
+    constructor(name, category, relase, payToWin, tags){
+        this._name = name,
+        this._category = category,
+        this._relase = relase,
+        this._payToWin = payToWin,
+        this._tags = tags
+    }
+
+    set name(value) {
+        this._name = value
+    }
+    set category(value) {
+        this._category = value
+    }
+    set relase(value) {
+        
+        this._relase = value
+    }
+    get name() {
+        this._name = 
+    }
+    get category() {
+        this._category = 
+    }
+    get relase() {
+        
+        this._relase = 
+    }
+
+}
+
+class MobileGame extends ComputerGame {
+    constructor(name, category, relase, payToWin, tags) {
+        super(name, category, relase, payToWin, tags);
+        this._is_mobile = true;
+    }
+}
+
+class AndroidGame extends MobileGame {
+    constructor(name, category, relase, payToWin, tags, is_mobile) {
+        super(name, category, relase, payToWin, tags, is_mobile);
+        this._is_android = true;
+    }
+}
+
+let fifa_and = new AndroidGame("fifa19", "sport", 2018, false, ['ps4', 'xbox', 'esport'])
+let fifa_mob = new MobileGame("fifa19", "sport", 2018, false, ['ps4', 'xbox', 'esport'])
+let fifa = new ComputerGame("fifa19", "sport", 2018, false, ['ps4', 'xbox', 'esport'])
+console.log(fifa)
+console.log(fifa_and)
+console.log(fifa_mob)
