5f37066ee77f0976f09540f5944223beaf903f54
mlabuda2 <labuda1996@wp.pl>
2019-04-27 11:25:11 +0200

    added action functions

--- lab5/src/es6_computerGame.js
+++ lab5/src/es6_computerGame.js
@@ -1,5 +1,4 @@
 class ComputerGame {
-
     constructor(name, category, relase, payToWin, tags){
         this._name = name,
         this._category = category,
@@ -7,7 +6,6 @@ class ComputerGame {
         this._payToWin = payToWin,
         this._tags = tags
     }
-
     set name(value) {
         this._name = value
     }
@@ -15,7 +13,6 @@ class ComputerGame {
         this._category = value
     }
     set relase(value) {
-        
         this._relase = value
     }
     get name() {
@@ -27,26 +24,47 @@ class ComputerGame {
     get relase() {
         return this._relase 
     }
-
+    runGame() {
+        console.log(`Uruchamiam ${this.name}`)
+    }
 }
 
 class MobileGame extends ComputerGame {
     constructor(name, category, relase, payToWin, tags) {
         super(name, category, relase, payToWin, tags);
+        this._screen_horizontally = false;
         this._is_mobile = true;
     }
-}
-
-class AndroidGame extends MobileGame {
-    constructor(name, category, relase, payToWin, tags, is_mobile) {
-        super(name, category, relase, payToWin, tags, is_mobile);
-        this._is_android = true;
+    runGame() {
+        console.log(`Uruchamiam ${this.name} (mobile)`)
+    }
+    rotateScreen() {
+        if (this._screen_horizontally){
+            this._screen_horizontally = false;
+            console.log(`Obracam ekran pionowo ${this.name} (mobile)`)
+        } else {
+            this._screen_horizontally = true;
+            console.log(`Obracam ekran poziomo ${this.name} (mobile)`)
+        }
+        
     }
 }
 
-let fifa_and = new AndroidGame("fifa19", "sport", 2018, false, ['ps4', 'xbox', 'esport'])
+// class AndroidGame extends MobileGame {
+//     constructor(name, category, relase, payToWin, tags, is_mobile) {
+//         super(name, category, relase, payToWin, tags, is_mobile);
+//         this._is_android = true;
+//     }
+// }
+
+// let fifa_and = new AndroidGame("fifa19", "sport", 2018, false, ['ps4', 'xbox', 'esport'])
 let fifa_mob = new MobileGame("fifa19", "sport", 2018, false, ['ps4', 'xbox', 'esport'])
 let fifa = new ComputerGame("fifa19", "sport", 2018, false, ['ps4', 'xbox', 'esport'])
 console.log(fifa)
-console.log(fifa_and)
+// console.log(fifa_and)
 console.log(fifa_mob)
+fifa.runGame()
+// fifa.rotateScreen()
+fifa_mob.runGame()
+fifa_mob.rotateScreen()
+fifa_mob.rotateScreen()
