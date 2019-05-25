ec74d6a6410417947699f07c95268c405ed0e35a
mlabuda2 <labuda1996@wp.pl>
2019-05-25 11:36:07 +0200

    U from CRUD added

--- lab6-7-8/reactserver/server.js
+++ lab6-7-8/reactserver/server.js
@@ -41,10 +41,50 @@ app.delete('/api/cgames/:id', (req, res) => {
     });
     if (!deleted) {
         console.log("Item doesn't exist.")
-        return res.send({"message": "Item doesn't exist."})
+        res.send({"message": "Item doesn't exist."})
     }
 });
 
-
+app.put('/api/cgames/update/:id', (req, res) => {
+    console.log("UPDATE /api/cgames/update/:id")
+    const id = req.params.id;
+    const game = req.body.cgame;
+    const validated = validate_game(game)
+    let updated = false;
+    if (validated[0]){
+        cgames.forEach(function  (element) {
+            console.log(element)
+            if (element._id == id){
+                const idx = cgames.indexOf(element)
+                cgames[idx] = game;
+                updated = true;
+                res.send({"message": "Item updated."})
+            }
+        });
+    }
+    else {
+        res.send({"message": validated[1]})
+    }
+    if (!updated) {
+        console.log("Item doesn't exist.")
+        res.send({"message": "Item doesn't exist."})
+    }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+const validate_game = function(game) {
+    if (!game){
+        return [false, "Not game provided"]}
+    if (!game.name) { 
+        return [false, "Not name provided."]}
+    if (!game.category) { 
+        return [false, "Not category provided."]}
+    if (!game.relase) { 
+        return [false, "Not relase provided."]}
+    if (!game.payToWin & game.payToWin != false) { 
+        return [false, "Not payToWin provided."]}
+    if (!game.tags) { 
+        return [false, "Not tags provided."]}
+    return [true, "OK"]
+}
